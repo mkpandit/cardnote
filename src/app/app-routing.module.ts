5e1b7a8ab503384ae66253d0b848d85c8ec71266
mkpandit <manish.kumer.pandit@gmail.com>
2018-03-17 14:05:03 -0400

    Add router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddcardComponent } from './addcard/addcard.component';
+import { CardComponent } from './card/card.component';
+
+const routes: Routes = [
+	{path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{path: 'dashboard', component: CardComponent },
+	{path: 'addcard', component: AddcardComponent }
+];
+
+@NgModule({
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
