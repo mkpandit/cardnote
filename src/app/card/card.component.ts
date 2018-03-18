7ab7fe7fb4b9b4d41b4d4c1ab6b9b3bbd52fbf93
mkpandit <manish.kumer.pandit@gmail.com>
2018-03-17 23:49:53 -0400

    Refactoring card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,19 +10,6 @@ import { CardserviceService } from './serve/cardservice.service';
 })
 export class CardComponent implements OnInit {
 	
-	cards = [
-		{ id: 11, name: 'Card 11', description: 'This is description for 11', exe_date: '2018-0101' },
-		{ id: 12, name: 'Card 12', description: 'This is description for 12', exe_date: '2018-0101' },
-		{ id: 13, name: 'Card 13', description: 'This is description for 13', exe_date: '2018-0101' },
-		{ id: 14, name: 'Card 14', description: 'This is description for 14', exe_date: '2018-0101' },
-		{ id: 15, name: 'Card 15', description: 'This is description for 15', exe_date: '2018-0101' },
-		{ id: 16, name: 'Card 16', description: 'This is description for 16', exe_date: '2018-0101' },
-		{ id: 17, name: 'Card 17', description: 'This is description for 17', exe_date: '2018-0101' },
-		{ id: 18, name: 'Card 18', description: 'This is description for 18', exe_date: '2018-0101' },
-		{ id: 19, name: 'Card 19', description: 'This is description for 19', exe_date: '2018-0101' },
-		{ id: 20, name: 'Card 20', description: 'This is description for 20', exe_date: '2018-0101' }
-	];
-	
 	selectedCard: Card;
 	
 	cardNoteListArray: any[];
@@ -45,13 +32,6 @@ export class CardComponent implements OnInit {
 		this.selectedCard = card;
 	}
 	
-	addCard(cardId, cardName, cardDescription, cardExeDate) {
-		this.cardService.addCardNote(cardId.value, cardName.value, cardDescription.value, cardExeDate.value);
-		cardId.value = null;
-		cardName.value = null;
-		cardDescription.value = null;
-		cardExeDate.value = null;
-	}
 	deleteCard($key: string) {
 		if(confirm("Are you sure you want to delete this card?")){
 			this.cardService.deleteCardNote($key);
